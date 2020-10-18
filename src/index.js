c9b56fd9b48b55e110a14cd9320beab6b0961054
Pavel Samovich <pavel.samovich@gmail.com>
2020-10-18 22:38:43 +0300

    feat: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+function ascendingComparator() {
+    return (a, b) => b - a;
+}
+
+function descendingComparator() {
+    return (a, b) => a - b;
+}
+
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    return matrix.reduce((previous, current, index) => {
+        const isReversed = index % 2 !== 0;
+        const compareFunction = isReversed ? ascendingComparator() : descendingComparator();
+        previous.push(...current.sort(compareFunction));
+        return previous;
+    }, []);
 }
